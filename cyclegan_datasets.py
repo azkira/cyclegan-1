63b88cccd5a85cea87b1097979341d835804e5ee
azkira <azkirasyid@gmail.com>
2022-04-11 13:51:49 +0700

    update-dataset-settings

--- cyclegan_datasets.py
+++ cyclegan_datasets.py
@@ -3,18 +3,18 @@
 """The size of each dataset. Usually it is the maximum number of images from
 each domain."""
 DATASET_TO_SIZES = {
-    'horse2zebra_train': 1334,
-    'horse2zebra_test': 140
+    'horse2zebra_train': 88,
+    'horse2zebra_test': 100
 }
 
 """The image types of each dataset. Currently only supports .jpg or .png"""
 DATASET_TO_IMAGETYPE = {
-    'horse2zebra_train': '.jpg',
+    'horse2zebra_train': '.png',
     'horse2zebra_test': '.jpg',
 }
 
 """The path to the output csv file."""
 PATH_TO_CSV = {
-    'horse2zebra_train': './CycleGAN_TensorFlow/input/horse2zebra/horse2zebra_train.csv',
-    'horse2zebra_test': './CycleGAN_TensorFlow/input/horse2zebra/horse2zebra_test.csv',
+    'horse2zebra_train': './CycleGAN_TensorFlow/input/sketch2photo/sketch2photo_train.csv',
+    'horse2zebra_test': './CycleGAN_TensorFlow/input/sketch2photo/sketch2photo_test.csv',
 }
